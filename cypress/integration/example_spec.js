e490c98cdb4736527a231e939d49f9e178e5f7d5
Chas Fricke <chasfricke@Chass-MacBook-Pro-2.local>
2018-02-19 10:33:59 -0700

    gitignore added

--- cypress/integration/example_spec.js
+++ cypress/integration/example_spec.js
@@ -1,16 +1,3 @@
-//
-// **** Kitchen Sink Tests ****
-//
-// This app was developed to demonstrate
-// how to write tests in Cypress utilizing
-// all of the available commands
-//
-// Feel free to modify this spec in your
-// own application as a jumping off point
-
-// Please read our "Introduction to Cypress"
-// https://on.cypress.io/introduction-to-cypress
-
 describe('Kitchen Sink', function () {
   it('.should() - assert that <title> is correct', function () {
     // https://on.cypress.io/visit
