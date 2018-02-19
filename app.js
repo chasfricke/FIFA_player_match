990460520eada93f31fb624f3e0ff768f3db10c4
Chas Fricke <chasfricke@Chass-MacBook-Pro-2.local>
2018-02-18 19:38:58 -0700

    final, minus testing

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 let fifaSkillAvg = {};
 let savedSkillScores = {};
 let skillSelected;
-let skillsList;
 let playerData;
 
 getPlayerData()
@@ -68,13 +67,19 @@ function getPlayerData() {
 }
 
 //User Skill Input
+let sliderContainer = document.querySelector(".slider-container")
+sliderContainer.style.display = 'none'
+
 let slider = document.querySelector(".slider")
-let sliderValueLabel = document.querySelector(".sliderValue")
+
+let sliderValueLabel = document.querySelector(".slider-value")
 sliderValueLabel.textContent = slider.value
 slider.addEventListener("mouseup", function() {
   sliderValueLabel.textContent = slider.value
 })
-
+slider.addEventListener("keydown", function() {
+  sliderValueLabel.textContent = slider.value
+})
 
 buildList();
 //Button Events
@@ -93,6 +98,7 @@ function buildButton(skill){
   newButton.addEventListener("click", function() {
     buildSkillCard(skill)
     skillSelected = skill;
+    sliderContainer.style.display = ''
   })
   var buttonContainer = document.querySelector(".skill-button-container")
   buttonContainer.appendChild(newButton)
@@ -101,35 +107,14 @@ function buildButton(skill){
 //Save Button
 document.querySelector(".save").addEventListener("click", function() {
   if (skillSelected != undefined){
+    sliderContainer.style.display = 'none'
     savedSkillScores[skillSelected] = parseInt(slider.value)
-    document.querySelector(".savedScore").textContent = slider.value
-    console.log(skillSelected)
     var skillArray = document.getElementsByClassName(skillSelected)
     skillArray[0].classList.add("active")
+
   }
-})
 
-
-
-//Building Skill Card
-function buildSkillCard (skill) {
-  skillSelected = skill
-  let title = document.querySelector(".skillCardTitle")
-  let skillAvg = document.querySelector(".skillAvg")
-  let savedScore = document.querySelector(".savedScore")
-  title.textContent = skill.charAt(0).toUpperCase() + skill.slice(1);
-  skillAvg.textContent = fifaSkillAvg[skill]
-  savedScore.textContent = savedSkillScores[skill]
-  if (savedSkillScores[skill] != undefined){
-    slider.value = savedSkillScores[skill]
-  }else {
-    slider.value = fifaSkillAvg[skill]
-  }
-  sliderValueLabel.textContent = slider.value
-}
-
-//match-button
-document.querySelector(".match-button").addEventListener("click", function() {
+  //Match Logic
   var playerScoreDifference = playerData.reduce((acc,el) => {
     var keys = Object.keys(savedSkillScores)
     acc[el.name] = keys.reduce((acc,el2) => {
@@ -142,6 +127,21 @@ document.querySelector(".match-button").addEventListener("click", function() {
   sortResults(playerScoreDifference);
 })
 
+//Building Skill Card
+function buildSkillCard (skill) {
+  skillSelected = skill
+  let title = document.querySelector(".skill-title")
+  let skillAvg = document.querySelector(".skillAvg")
+  title.textContent = skill.charAt(0).toUpperCase() + skill.slice(1);
+  skillAvg.textContent = fifaSkillAvg[skill]
+  if (savedSkillScores[skill] != undefined){
+    slider.value = savedSkillScores[skill]
+  }else {
+    slider.value = fifaSkillAvg[skill]
+  }
+  sliderValueLabel.textContent = slider.value
+}
+
 //sort-results
 function sortResults(object){
   var list = object
@@ -153,11 +153,11 @@ function sortResults(object){
 }
 
 function displayResults(results){
-  console.log(savedSkillScores)
   var matchNames = document.querySelector(".matchNames")
   var matchScores = document.querySelector(".matchScores")
 
   var topPlayerName = document.querySelector(".player-name")
+  var topPlayerOverall = document.querySelector(".player-overall")
   var topPlayerPositions = document.querySelector(".player-position")
   var topPlayerClub = document.querySelector(".player-club")
   var topPlayerImg = document.querySelector(".player-photo")
@@ -169,23 +169,58 @@ function displayResults(results){
     topPlayerPositions.innerHTML = "";
     topPlayerClub.innerHTML = "";
     topPlayerImg.innerHTML = "";
+    topPlayerOverall.innerHTML = "";
   }
+
+  //Top Player Data
   topPlayerName.innerHTML = results[0][0]
   var topPlayerData = playerData.filter(data => data.name == results[0][0])
-
-
   topPlayerImg.src = topPlayerData[0].photo
-    console.log(topPlayerImg)
-  topPlayerPositions.innerHTML = topPlayerData[0].preferredPositions
-  topPlayerClub.innerHTML = topPlayerData[0].club
+  topPlayerOverall.innerHTML = "OVERALL: " + topPlayerData[0].overall
+  topPlayerPositions.innerHTML = "POSITION: " + topPlayerData[0].preferredPositions
+  topPlayerClub.innerHTML = "CLUB: " + topPlayerData[0].club
 
+  //create topPlayerData array
+  var topPlayerDataArray = [];
+  var topPlayerDataKeys = Object.keys(topPlayerData[0]);
+  var topPlayerDataValues = Object.values(topPlayerData[0]);
+  for (let a = 0; a < 40; a++){
+    topPlayerDataArray.push(topPlayerDataKeys[a] + ":   " + topPlayerDataValues[a])
+  }
 
+  //remaining top player data
+  var remainingSkillsList = document.querySelector(".remaining-skills-list")
+  remainingSkillsList.innerHTML = "";
+  for (let x = 10; x < 39; x++){
+    var skillKeyPair = topPlayerDataArray[x]
+    var skillLi = document.createElement("li")
+    skillLi.textContent = (skillKeyPair)
+    remainingSkillsList.appendChild(skillLi)
+  }
+
+  //user score data displayResults
+    var savedSkillsList = document.querySelector(".saved-skills-list")
+    var savedSkillScoresArray = [];
+    var savedSkillScoresKeys = Object.keys(savedSkillScores);
+    var savedSkillScoresValues = Object.values(savedSkillScores);
+    var totalSavedScore = 0;
+    savedSkillsList.innerHTML = "";
+    for (let z = 0; z < savedSkillScoresKeys.length; z++){
+      savedSkillScoresArray.push(savedSkillScoresKeys[z] + ": " + savedSkillScoresValues[z])
+      totalSavedScore = totalSavedScore + Number(savedSkillScoresValues[z])
+      var skillKeyPair = savedSkillScoresArray[z]
+      var skillLi = document.createElement("li")
+      skillLi.textContent = (skillKeyPair)
+      savedSkillsList.appendChild(skillLi)
+    }
+
+  //Top 10 Matches
   for (let i = 0; i < 10; i++){
     var namesLi = document.createElement("li")
     namesLi.textContent = (results[i][0])
     matchNames.appendChild(namesLi)
     var scoresLi = document.createElement("li")
-    scoresLi.textContent = (results[i][1])
+    scoresLi.textContent = Math.floor(((totalSavedScore) - (results[i][1])) / totalSavedScore * 100)
     matchScores.appendChild(scoresLi)
   }
 }
